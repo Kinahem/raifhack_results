c52d6e40dab9d8828afc80cd921d758605e98a62
Myhaylo Turkin <myhaylo.turkin@competera.net>
2021-09-26 02:23:18 +0300

    wo mult catb and hack data

---
+++ Raif/avg_prediction.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df1 = pd.read_csv('catb_ot_no_mult_stkfold_10cl.csv', index_col='id')
+df2 = pd.read_csv('submission0.1266_catb.csv', index_col='id')
+df3 = pd.read_csv('submit_full_featurs_with_external_data_lgbm.csv', index_col='id')
+
+final_df = (df1+df2/0.9+df3)/3
+final_df_09 = final_df*0.9
+
+final_df.to_csv('final_sub.csv')
+final_df_09.to_csv('final_sub_09.csv')
